b62965905b2d2003612490db9f610902fbccab3a
yauhenmakarevich <yauhenmakarevich@gmail.com>
2023-05-07 22:36:20 +0300

    feat: add 02-write-file solution

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,3 +2,20 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 let fileName = path.join(__dirname, 'text.txt');
+let outputFile = fs.createWriteStream(fileName);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('You can type something:\n');
+///Уточнить и проверить
+rl.on('line', (input) => {
+  if (input === 'exit') {
+    rl.close();
+  } else {
+    outputFile.write(input + '\n');
+  }
+});
+process.on('exit', () => console.log('\nGood luck!'));
+///Уточнить и проверить
