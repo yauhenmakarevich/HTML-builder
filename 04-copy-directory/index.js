dfa1618bf4e5cbfe4da6175d208dcf082cbd70d6
yauhenmakarevich <yauhenmakarevich@gmail.com>
2023-05-08 17:49:54 +0300

    feat: add copy function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -17,5 +17,24 @@ fs.mkdir(copyFolderName, { recursive: true }, (err) => {
 fs.readdir(folderName, { withFileTypes: true }, (err, files) => {
   if (err) console.log(err);
 
+  async function copyFiles(files, allCopyFiles) {
+    await allCopyFiles.forEach((file) => {
+      let copyFileName = path.join(__dirname, 'files-copy', file);
+      fsPromises.unlink(copyFileName);
+    });
 
-  // Асинхроная функция,Ожидане,чтение
+      await files.forEach((file) => {
+      let fileName = path.join(__dirname, 'files', file.name);
+      let copyFileName = path.join(__dirname, 'files-copy', file.name);
+      fsPromises.copyFile(fileName, copyFileName);
+    });
+  }
+
+   fs.readdir(copyFolderName, (err, allCopyFiles) => {
+    if (err) console.log(err);
+    copyFiles(files, allCopyFiles);
+  });
+});
+
+
+  //Ожидане,чтение
