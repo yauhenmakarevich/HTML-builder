ecffd4cd248993221e2e63b5067787b7578a4a2f
yauhenmakarevich <yauhenmakarevich@gmail.com>
2023-05-08 17:54:16 +0300

    init: start and add 05-merge-styles solution

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,3 +1,14 @@
 // Привет дорогой проверяющий, есть большая вероятность того что изза праздников я могу не успеть доделать .
 // Пожалуста проверь работу в среду вечером .
 //Или оставь для связи свой ник в дискорд ....
+
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
+let folderName = path.join(__dirname, 'styles');
+fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
+
+
+//функция ... чтение
+
+readFiles();
