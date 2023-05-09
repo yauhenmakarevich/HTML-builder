a7f71f89c794fbac2da561e763164699fcc306e0
yauhenmakarevich <yauhenmakarevich@gmail.com>
2023-05-09 09:42:29 +0300

    feat: add function solution and add project-dist

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -8,7 +8,23 @@ const path = require('path');
 let folderName = path.join(__dirname, 'styles');
 fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'));
 
+  async function readFiles() {
+  const cssFiles = await fsPromises.readdir(folderName, {
+    withFileTypes: true,
+  });
 
-//функция ... чтение
+  async function readFile(fileName) {
+    let text = await fsPromises.readFile(fileName);
+    let outputFile = path.join(__dirname, 'project-dist', 'bundle.css');
+
+    await fsPromises.appendFile(outputFile, text.toString());
+  }
+
+    await cssFiles.forEach((file) => {
+      let fileName = path.join(__dirname, 'styles', file.name);
+      let ext = path.parse(fileName).ext.split('.')[1];
+      if (file.isFile() && ext === 'css') readFile(fileName);
+    });
+    }
 
 readFiles();
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,3 +1,22 @@
 // Привет дорогой проверяющий, есть большая вероятность того что изза праздников я могу не успеть доделать .
 // Пожалуста проверь работу в среду вечером .
 //Или оставь для связи свой ник в дискорд ....
+
+
+
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
+
+
+async function copyHtml() {
+  let componentsFolderName = path.join(__dirname, 'components');
+  let outHtmlfileName = path.join(__dirname, 'project-dist', 'index.html');
+
+
+
+
+async function readFile(file) {
+    let fileName = path.join(__dirname, 'components', file);
+    let name = file.split('.')[0];
+    let text = (await fsPromises.readFile(fileName)).toString();
